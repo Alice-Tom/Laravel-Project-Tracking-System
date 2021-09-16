eaf9a9f5ec9273e139b9dc0d7c4522839cf89a86
Alice Tom M <admachuve@gmail.com>
2021-09-16 15:47:30 +0300

    edied assets for registration

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,17 +6,17 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="description" content="Sportify template project">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="{{asset('styles/bootstrap-4.1.2/bootstrap.min.css')}}">
-<link href="{{ asset('plugins/font-awesome-4.7.0/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
-<link rel="stylesheet" type="text/css" href="{{asset('plugins/OwlCarousel2-2.3.4/owl.carousel.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('plugins/OwlCarousel2-2.3.4/owl.theme.default.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('plugins/OwlCarousel2-2.3.4/animate.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('styles/main_styles.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('styles/responsive.css')}}">
-<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/apple-touch-icon.png')}}?v={{ date('YmdHis') }}">
-<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon-32x32.png')}}?v={{ date('YmdHis') }}">
-<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon-16x16.png')}}?v={{ date('YmdHis') }}">
-<link rel="manifest" href="{{ asset('site.webmanifest')}}?v={{ date('YmdHis') }}">
+<link rel="stylesheet" type="text/css" href="{{secure_asset('styles/bootstrap-4.1.2/bootstrap.min.css')}}">
+<link href="{{ secure_asset('plugins/font-awesome-4.7.0/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="{{secure_asset('plugins/OwlCarousel2-2.3.4/owl.carousel.css')}}">
+<link rel="stylesheet" type="text/css" href="{{secure_asset('plugins/OwlCarousel2-2.3.4/owl.theme.default.css')}}">
+<link rel="stylesheet" type="text/css" href="{{secure_asset('plugins/OwlCarousel2-2.3.4/animate.css')}}">
+<link rel="stylesheet" type="text/css" href="{{secure_asset('styles/main_styles.css')}}">
+<link rel="stylesheet" type="text/css" href="{{secure_asset('styles/responsive.css')}}">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ secure_asset('images/apple-touch-icon.png')}}?v={{ date('YmdHis') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ secure_asset('images/favicon-32x32.png')}}?v={{ date('YmdHis') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ secure_asset('images/favicon-16x16.png')}}?v={{ date('YmdHis') }}">
+<link rel="manifest" href="{{ secure_asset('site.webmanifest')}}?v={{ date('YmdHis') }}">
 </head>
 <body>
 
@@ -247,18 +247,18 @@ Copyright &copy; <script>document.write(new Date().getFullYear());</script> All 
 	</footer>
 </div>
 
-<script src="{{asset('js2/jquery-3.3.1.min.js')}}"></script>
-<script src="{{asset('styles/bootstrap-4.1.2/popper.js')}}"></script>
-<script src="{{asset('styles/bootstrap-4.1.2/bootstrap.min.js')}}"></script>
-<script src="{{asset('plugins/greensock/TweenMax.min.js')}}"></script>
-<script src="{{asset('plugins/greensock/TimelineMax.min.js')}}"></script>
-<script src="{{asset('plugins/scrollmagic/ScrollMagic.min.js')}}"></script>
-<script src="{{asset('plugins/greensock/animation.gsap.min.js')}}"></script>
-<script src="{{asset('plugins/greensock/ScrollToPlugin.min.js')}}"></script>
-<script src="{{asset('plugins/OwlCarousel2-2.3.4/owl.carousel.js')}}"></script>
-<script src="{{asset('plugins/easing/easing.js')}}"></script>
-<script src="{{asset('plugins/progressbar/progressbar.min.js')}}"></script>
-<script src="{{asset('plugins/parallax-js-master/parallax.min.js')}}"></script>
-<script src="{{asset('js2/custom.js')}}"></script>
+<script src="{{secure_asset('js2/jquery-3.3.1.min.js')}}"></script>
+<script src="{{secure_asset('styles/bootstrap-4.1.2/popper.js')}}"></script>
+<script src="{{secure_asset('styles/bootstrap-4.1.2/bootstrap.min.js')}}"></script>
+<script src="{{secure_asset('plugins/greensock/TweenMax.min.js')}}"></script>
+<script src="{{secure_asset('plugins/greensock/TimelineMax.min.js')}}"></script>
+<script src="{{secure_asset('plugins/scrollmagic/ScrollMagic.min.js')}}"></script>
+<script src="{{secure_asset('plugins/greensock/animation.gsap.min.js')}}"></script>
+<script src="{{secure_asset('plugins/greensock/ScrollToPlugin.min.js')}}"></script>
+<script src="{{secure_asset('plugins/OwlCarousel2-2.3.4/owl.carousel.js')}}"></script>
+<script src="{{secure_asset('plugins/easing/easing.js')}}"></script>
+<script src="{{secure_asset('plugins/progressbar/progressbar.min.js')}}"></script>
+<script src="{{secure_asset('plugins/parallax-js-master/parallax.min.js')}}"></script>
+<script src="{{secure_asset('js2/custom.js')}}"></script>
 </body>
 </html>
