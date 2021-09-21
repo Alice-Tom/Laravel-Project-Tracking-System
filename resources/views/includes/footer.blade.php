0076cf0926b328f67953f9223092993dbdb7920c
Alice Tom M <admachuve@gmail.com>
2021-09-21 15:13:32 +0300

    db configuration

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,7 +3,7 @@
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
             <hr>
-            <span>Copyright &copy;  <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>document.write(new Date().getFullYear());</script> All rights reserved | <a href="index">Project Tracking System </a><br><br>Site made by ATM</span>
+            <span>Copyright &copy;  <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>document.write(new Date().getFullYear());</script> All rights reserved | <a href="/">Project Tracking System </a><br><br>Site made by ATM</span>
         </div>
     </div>
 </footer>
